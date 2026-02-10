e4ecccb182e66c891dabff1f17821773d34402b4
JuanManuelRivera31 <juanm.rivera3105@gmail.com>
2026-02-09 23:56:23 -0500

    En nuestra API no todas las recetas tienen un numero de ingredientes y porciones iguales, por lo tanto iteramos dinamicamente en el rango de los ingredientes y cantidades que tenga la receta en cuestión y rendeizamos correctamente, procedemos con favoritos

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react';
-import { Fragment } from 'react';
+import { Fragment, type JSX } from 'react';
 import { useAppStore } from '../stores/useAppStore';
 
 export default function Modal() {
@@ -8,6 +8,23 @@ export default function Modal() {
     const closeModal = useAppStore((state) => state.closeModal)
     const selectedRecipe = useAppStore((state) => state.selectedRecipe)
 
+  const renderIngredients = () => {
+    const ingredients : JSX.Element[] = []; 
+
+    for (let i = 1; i <= 6; i++) { //Iteramos dinamicamente en el rango de los posibles ingredientes y cantidades que permite la api
+      const ingredient = selectedRecipe[`strIngredient${i}` as keyof typeof selectedRecipe]; //Keyoff utiliza cualquiera de los atributos que pertenencen al type
+      const measure = selectedRecipe[`strMeasure${i}` as keyof typeof selectedRecipe];
+      
+      if (ingredient && measure) {
+        ingredients.push( //No se recomienda usar push en React, pero en este caso es una forma sencilla de agregar elementos a un array que luego se renderiza
+          <li key={i} className="text-lg font-normal">
+            {ingredient} - {measure}
+          </li>
+        );
+      }
+  }
+  return <ul className="my-5">{ingredients}</ul>;
+}
 return (
     <>
       <Transition appear show={modal} as={Fragment}>
@@ -21,7 +38,7 @@ return (
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-70" />
+            <div className="fixed inset-0 bg-black/80 backdrop:blur-md bg-opacity-70" />
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
@@ -49,11 +66,31 @@ return (
                   <Dialog.Title as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
                     Ingredientes y Cantidades
                   </Dialog.Title>
+
+                  {renderIngredients()}
+
                   <Dialog.Title as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
                     Instrucciones
                   </Dialog.Title>
 
                     <p className='text-lg'>{selectedRecipe.strInstructions}</p>
+
+                  <div className="mt-5 flex justify-between gap-4">
+                    <button
+                      type="button"
+                      className="w-full rounded bg-red-700 p-3 uppercase text-white font-bold shadow hover:bg-red-500"
+                      onClick={closeModal}
+                    >Cerrar
+                    </button>
+
+                    <button
+                      type="button"
+                      className="w-full rounded bg-orange-500 p-3 uppercase text-white font-bold shadow hover:bg-orange-400"
+                      onClick={closeModal}
+                    >Agregar a favoritos
+                    </button>
+
+                  </div>
 
                 </Dialog.Panel>
               </Transition.Child>
